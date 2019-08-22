7a3bb6a0db9dd5f078718a475b222e42cdc566e9
Abbey Masters <abbeykmasters@gmail.com>
2019-08-22 13:35:32 -0700

    checking

--- public/src/services/books.js
+++ public/src/services/books.js
@@ -1,68 +1,68 @@
-const books = [
-    {
-        title: 'Sometimes a Great Notion',
-        author: 'Ken Kesey',
-        year: 1964,
-        url: 'https://covers.powells.com/9780143039860.jpg',
-        genre: 'Local Literature',
-        available: true
-    },
-    {
-        title: 'All the Light We Cannot See',
-        author: 'Anthony Doer',
-        year: 2017,
-        url: 'https://images-na.ssl-images-amazon.com/images/I/51tjJGTkUhL._SX326_BO1,204,203,200_.jpg',
-        genre: 'Historical Fiction',
-        available: true
-    },
-    {
-        title: 'The Goldfinch',
-        author: 'Donna Tartt',
-        year: 2015,
-        url: 'https://images-na.ssl-images-amazon.com/images/I/41p4amaYISL._SX317_BO1,204,203,200_.jpg',
-        genre: 'Literary Fiction',
-        available: true
-    },
-    {
-        title: 'Cold Lake',
-        author: 'Jeff Carson',
-        year: 2014,
-        url: 'https://images-na.ssl-images-amazon.com/images/I/41axtSd5YzL._SX326_BO1,204,203,200_.jpg',
-        genre: 'Mystery',
-        available: true
-    },
-    {
-        title: 'I Am Watching You',
-        author: 'Teresa Driscoll',
-        year: 2017,
-        url: 'https://images-na.ssl-images-amazon.com/images/I/41m0qkxUg%2BL._SX323_BO1,204,203,200_.jpg',
-        genre: 'Mystery',
-        available: false
-    },
-    {
-        title: 'Harry Potter and the Prisoner of Azkaban',
-        author: 'J.K. Rowling',
-        year: 1999,
-        url: 'https://images-na.ssl-images-amazon.com/images/I/51NuYi4-XoL.jpg',
-        genre: 'Young Adult',
-        available: false
-    },
-    {
-        title: 'Small Victories',
-        author: 'Julia Turshen',
-        year: 2016,
-        url: 'https://images-na.ssl-images-amazon.com/images/I/51PFFafl3jL._SX401_BO1,204,203,200_.jpg',
-        genre: 'Cooking',
-        available: true
-    },
-    {
-        title: 'Educated',
-        author: 'Tara Westover',
-        year: 2018,
-        url: 'https://images-na.ssl-images-amazon.com/images/I/41%2BaN7ZbS9L._SX327_BO1,204,203,200_.jpg',
-        genre: 'Memoir',
-        available: true
-    },
-]
+// const books = [
+//     {
+//         title: 'Sometimes a Great Notion',
+//         author: 'Ken Kesey',
+//         year: 1964,
+//         url: 'https://covers.powells.com/9780143039860.jpg',
+//         genre: 'Local Literature',
+//         available: true
+//     },
+//     {
+//         title: 'All the Light We Cannot See',
+//         author: 'Anthony Doer',
+//         year: 2017,
+//         url: 'https://images-na.ssl-images-amazon.com/images/I/51tjJGTkUhL._SX326_BO1,204,203,200_.jpg',
+//         genre: 'Historical Fiction',
+//         available: true
+//     },
+//     {
+//         title: 'The Goldfinch',
+//         author: 'Donna Tartt',
+//         year: 2015,
+//         url: 'https://images-na.ssl-images-amazon.com/images/I/41p4amaYISL._SX317_BO1,204,203,200_.jpg',
+//         genre: 'Literary Fiction',
+//         available: true
+//     },
+//     {
+//         title: 'Cold Lake',
+//         author: 'Jeff Carson',
+//         year: 2014,
+//         url: 'https://images-na.ssl-images-amazon.com/images/I/41axtSd5YzL._SX326_BO1,204,203,200_.jpg',
+//         genre: 'Mystery',
+//         available: true
+//     },
+//     {
+//         title: 'I Am Watching You',
+//         author: 'Teresa Driscoll',
+//         year: 2017,
+//         url: 'https://images-na.ssl-images-amazon.com/images/I/41m0qkxUg%2BL._SX323_BO1,204,203,200_.jpg',
+//         genre: 'Mystery',
+//         available: false
+//     },
+//     {
+//         title: 'Harry Potter and the Prisoner of Azkaban',
+//         author: 'J.K. Rowling',
+//         year: 1999,
+//         url: 'https://images-na.ssl-images-amazon.com/images/I/51NuYi4-XoL.jpg',
+//         genre: 'Young Adult',
+//         available: false
+//     },
+//     {
+//         title: 'Small Victories',
+//         author: 'Julia Turshen',
+//         year: 2016,
+//         url: 'https://images-na.ssl-images-amazon.com/images/I/51PFFafl3jL._SX401_BO1,204,203,200_.jpg',
+//         genre: 'Cooking',
+//         available: true
+//     },
+//     {
+//         title: 'Educated',
+//         author: 'Tara Westover',
+//         year: 2018,
+//         url: 'https://images-na.ssl-images-amazon.com/images/I/41%2BaN7ZbS9L._SX327_BO1,204,203,200_.jpg',
+//         genre: 'Memoir',
+//         available: true
+//     },
+// ]
 
-export default books;
+// export default books;
