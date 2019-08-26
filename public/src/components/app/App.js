bb695bac35dcfc02ee63abc0f53e60e094a56fb8
Abbey Masters <abbeykmasters@gmail.com>
2019-08-26 11:17:20 -0700

    finished homepage design

--- public/src/components/app/App.js
+++ public/src/components/app/App.js
@@ -6,6 +6,28 @@ class App extends Component {
     onRender(dom) {
         const header = new Header();
         dom.prepend(header.renderDOM());
+
+        const headerOne = document.createElement('h1');
+        const main = dom.querySelector('main');
+        headerOne.textContent = 'Welcome to Full Stacks Library';
+        main.prepend(headerOne);
+
+        const blurb = document.createElement('p');
+        blurb.classList.add('blurb');
+        blurb.textContent = 'Check out our online library for the best online book selections available.';
+        main.appendChild(blurb);
+
+        const button = document.createElement('button');
+        const p = document.createElement('p');
+        p.classList.add('blurb');
+        button.innerHTML = 'View Our Book List';
+        main.appendChild(p);
+        p.appendChild(button);
+
+        button.addEventListener('click', () => {
+            window.location = 'book-list.html';
+        });
+
     }
 
     renderHTML() {
